9dc37e15c9af72836d9eade6a03332b3ce55e171
Milan Straka <fox@ucw.cz>
2014-02-17 17:15:24 +0100

    Change options, add tokenization to xml output format.
    - Allow chossing input and output format separately.
    
    - The xml output format includes tokenization information.
    
    - Always sort recognized named entities.

--- src/run_ner.cpp
+++ src/run_ner.cpp
@@ -16,6 +16,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with NameTag.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <algorithm>
 #include <cstring>
 #include <ctime>
 #include <memory>
@@ -23,57 +24,53 @@
 #include "ner/ner.h"
 #include "utils/input.h"
 #include "utils/output.h"
+#include "utils/parse_options.h"
 #include "utils/process_args.h"
 
 using namespace ufal::nametag;
 
-static void recognize_vertical(FILE* in, FILE* out, const ner& recognizer);
-static void recognize_untokenized(FILE* in, FILE* out, const ner& recognizer);
+static void sort_entities(vector<named_entity>& entities);
+static void recognize_vertical(FILE* in, FILE* out, const ner& recognizer, tokenizer& tokenizer);
+static void recognize_untokenized(FILE* in, FILE* out, const ner& recognizer, tokenizer& tokenizer);
 
 int main(int argc, char* argv[]) {
-  bool use_vertical = false;
-
-  int argi = 1;
-  if (argi < argc && strcmp(argv[argi], "-v") == 0) argi++, use_vertical = true;
-  if (argi >= argc) runtime_errorf("Usage: %s [-v] ner_model", argv[0]);
+  options_map options;
+  if (!parse_options({{"input",{"untokenized", "vertical"}},
+                      {"output",{"vertical","xml"}}}, argc, argv, options) ||
+      argc < 2)
+    runtime_errorf("Usage: %s [options] recognizer_model [file[:output_file]]...\n"
+                   "Options: --input=untokenized|vertical\n"
+                   "         --output=vertical|xml", argv[0]);
 
   eprintf("Loading ner: ");
-  unique_ptr<ner> recognizer(ner::load(argv[argi]));
-  if (!recognizer) runtime_errorf("Cannot load ner from file '%s'!", argv[argi]);
+  unique_ptr<ner> recognizer(ner::load(argv[1]));
+  if (!recognizer) runtime_errorf("Cannot load ner from file '%s'!", argv[1]);
   eprintf("done\n");
-  argi++;
 
-  eprintf("Recognizing: ");
+  unique_ptr<tokenizer> tokenizer(options.count("input") && options["input"] == "vertical" ? tokenizer::new_vertical_tokenizer() : recognizer->new_tokenizer());
+  if (!tokenizer) runtime_errorf("No tokenizer is defined for the supplied model!");
+
   clock_t now = clock();
-  if (use_vertical) process_args(argi, argc, argv, recognize_vertical, *recognizer);
-  else process_args(argi, argc, argv, recognize_untokenized, *recognizer);
-  eprintf("done, in %.3f seconds.\n", (clock() - now) / double(CLOCKS_PER_SEC));
+  if (options.count("output") && options["output"] == "vertical")  process_args(2, argc, argv, recognize_vertical, *recognizer, *tokenizer);
+  else process_args(2, argc, argv, recognize_untokenized, *recognizer, *tokenizer);
+  eprintf("Recognizing done, in %.3f seconds.\n", (clock() - now) / double(CLOCKS_PER_SEC));
 
   return 0;
 }
 
-void recognize_vertical(FILE* in, FILE* out, const ner& recognizer) {
-  string line;
-  unsigned total_lines = 0;
-
-  vector<string> words;
+void recognize_vertical(FILE* in, FILE* out, const ner& recognizer, tokenizer& tokenizer) {
+  string para;
   vector<string_piece> forms;
   vector<named_entity> entities;
+  unsigned total_tokens = 0;
   string entity_ids, entity_text;
 
-  for (bool not_eof = true; not_eof; ) {
-    // Read sentence
-    words.clear();
-    forms.clear();
-    while ((not_eof = getline(in, line)) && !line.empty()) {
-      auto tab = line.find('\t');
-      words.emplace_back(tab == string::npos ? line : line.substr(0, tab));
-      forms.emplace_back(words.back());
-    }
-
-    // Find named entities in the sentence
-    if (!forms.empty()) {
+  while (getpara(in, para)) {
+    // Tokenize and tag
+    tokenizer.set_text(para);
+    while (tokenizer.next_sentence(&forms, nullptr)) {
       recognizer.recognize(forms, entities);
+      sort_entities(entities);
 
       for (auto&& entity : entities) {
         entity_ids.clear();
@@ -83,57 +80,67 @@ void recognize_vertical(FILE* in, FILE* out, const ner& recognizer) {
             entity_ids += ',';
             entity_text += ' ';
           }
-          entity_ids += to_string(total_lines + i + 1);
-          entity_text += words[i];
+          entity_ids += to_string(total_tokens + i + 1);
+          entity_text.append(forms[i].str, forms[i].len);
         }
         fprintf(out, "%s\t%s\t%s\n", entity_ids.c_str(), entity.type.c_str(), entity_text.c_str());
       }
+      total_tokens += forms.size() + 1;
     }
-
-    total_lines += forms.size() + 1;
   }
 }
 
-void recognize_untokenized(FILE* in, FILE* out, const ner& recognizer) {
+void recognize_untokenized(FILE* in, FILE* out, const ner& recognizer, tokenizer& tokenizer) {
   string para;
   vector<string_piece> forms;
   vector<named_entity> entities;
-  vector<const char*> entity_ends;
-
-  unique_ptr<tokenizer> tokenizer(recognizer.new_tokenizer());
-  if (!tokenizer) runtime_errorf("No tokenizer is defined for the supplied model!");
+  vector<size_t> entity_ends;
 
   while (getpara(in, para)) {
     // Tokenize the text and find named entities
-    tokenizer->set_text(para);
+    tokenizer.set_text(para);
     const char* unprinted = para.c_str();
-    while (tokenizer->next_sentence(&forms, nullptr)) {
+    while (tokenizer.next_sentence(&forms, nullptr)) {
       recognizer.recognize(forms, entities);
-      for (auto&& entity : entities) {
-        // Close entities that end sooned than current entity
-        while (!entity_ends.empty() && entity_ends.back() < forms[entity.start].str) {
-          if (unprinted < entity_ends.back()) print_xml_content(out, unprinted, entity_ends.back() - unprinted);
-          unprinted = entity_ends.back();
-          entity_ends.pop_back();
-          fputs("</ne>", out);
+      sort_entities(entities);
+
+      for (unsigned i = 0, e = 0; i < forms.size(); i++) {
+        if (unprinted < forms[i].str) print_xml_content(out, unprinted, forms[i].str - unprinted);
+        if (i == 0) fputs("<sentence>", out);
+
+        // Open entities starting at current token
+        for (; e < entities.size() && entities[e].start == i; e++) {
+          fprintf(out, "<ne type=\"%s\">", entities[e].type.c_str());
+          entity_ends.push_back(entities[e].start + entities[e].length - 1);
         }
 
-        // Print text just before the entity, open it and add end to the stack
-        if (unprinted < forms[entity.start].str) print_xml_content(out, unprinted, forms[entity.start].str - unprinted);
-        unprinted = forms[entity.start].str;
-        fprintf(out, "<ne type=\"%s\">", entity.type.c_str());
-        entity_ends.push_back(forms[entity.start + entity.length - 1].str + forms[entity.start + entity.length - 1].len);
-      }
+        // The token itself
+        fputs("<token>", out);
+        print_xml_content(out, forms[i].str, forms[i].len);
+        fputs("</token>", out);
 
-      // Close unclosed entities
-      while (!entity_ends.empty()) {
-        if (unprinted < entity_ends.back()) print_xml_content(out, unprinted, entity_ends.back() - unprinted);
-        unprinted = entity_ends.back();
-        entity_ends.pop_back();
-        fputs("</ne>", out);
+        // Close entities ending after current token
+        while (!entity_ends.empty() && entity_ends.back() == i) {
+          fputs("</ne>", out);
+          entity_ends.pop_back();
+        }
+        if (i + 1 == forms.size()) fputs("</sentence>", out);
+        unprinted = forms[i].str + forms[i].len;
       }
     }
     // Write rest of the text (should be just spaces)
     if (unprinted < para.c_str() + para.size()) print_xml_content(out, unprinted, para.c_str() + para.size() - unprinted);
   }
+}
+
+void sort_entities(vector<named_entity>& entities) {
+  struct named_entity_comparator {
+    static bool lt(const named_entity& a, const named_entity& b) {
+      return a.start < b.start || (a.start == b.start && a.length > b.length);
+    }
+  };
+
+  // Many models return entities sorted -- it is worthwhile to check that.
+  if (!is_sorted(entities.begin(), entities.end(), named_entity_comparator::lt))
+    sort(entities.begin(), entities.end(), named_entity_comparator::lt);
 }
