a5793636cc45af6f4e2b251abd04386248db8395
Milan Straka <milan@strakovi.com>
2016-05-05 21:23:35 +0200

    Bump version to 1.1.2-devel.

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -17,7 +17,7 @@ namespace ufal {
 namespace nametag {
 
 version version::current() {
-  return {1, 1, 1, ""};
+  return {1, 1, 2, "devel"};
 }
 
 // Returns multi-line formated version and copyright string.
